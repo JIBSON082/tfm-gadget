cfafa7d21377d2fc3834224990cb8e9d014fc8b2
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-09-04 19:39:30 +0100

    Create Smoothscroll.tsx

---
+++ components/Smoothscroll.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function SmoothScroll({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.0,
+      easing: (t: number) => 1 - Math.pow(1 - t, 3),
+      smoothWheel: true,
+      touchMultiplier: 1.2,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
