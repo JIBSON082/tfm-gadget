78fb56c2831b3a6d47eabba396c2dc2921075752
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-09-04 19:49:57 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Nav from "@/components/Nav";
 import Hero from "@/components/Hero";
 import ProductShowcase from "@/components/ProductShowcase";
 
 export default function Home() {
   return (
     <main>
+      <Nav />
       <Hero />
       <ProductShowcase />
     </main>
